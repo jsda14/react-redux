1b4464e353e09b6458091581d289584d3a2c5866
jsda14 <jsda14@gmail.com>
2024-04-09 16:43:55 -0500

    Create title

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -2,15 +2,18 @@ import { StarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
 
-function PokemonCard ({pokemon}){
-	return(
-		<Card
-			title={pokemon.name}
-			cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png" alt="Ditto"/>}
-			extra={<StarOutlined/>}
-		>
-			<Meta description='fire, magic'/>
-		</Card>
+function PokemonCard({ pokemon }) {
+	return (
+		<>
+			<h1> HOLA </h1>
+			<Card
+				title={pokemon.name}
+				cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png" alt="Ditto" />}
+				extra={<StarOutlined />}
+			>
+				<Meta description='fire, magic' />
+			</Card>
+		</>
 	)
 }
 
